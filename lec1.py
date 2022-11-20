c52284524f3c9c58b5938567589c6a879bc74338
Daria Dychek <dychek.d@gmail.com>
2022-11-20 20:43:55 +0100

    first lecture

---
+++ lec1.py
@@ -0,0 +1,30 @@
+#print('hello, world!')
+#a = 123
+#b = 1.23
+#print (a, b)
+#f = None
+#print(type(f))
+#f = 1
+#print(f)
+#print('{1} - {2} - {0}'.format(a, b, f))
+#list  = [1,2,3]
+#print(list)
+#print('enter d')
+#d = input()
+#print(d)
+# a = 10
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# c = 1.3
+# print(round(b*c))
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a>b:
+#     print(a)
+# else:
+#     print(b)
+for i in 1,2,3,4,5:
+    print(i**2)
