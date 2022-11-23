f5428fcfee51242ab522c8054c2ae5ae2d8de992
Daria Dychek <dychek.d@gmail.com>
2022-11-23 16:44:54 +0100

    coordinate axes quarters

---
+++ hwtask3.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+# в которой находится эта точка (или на какой оси она находится).
+
+x = float(input('enter x ' ))
+y = float(input('enter y ' ))
+
+if x > 0 and y > 0:
+    print ('1')
+elif x < 0 and y > 0:
+    print ('2')
+elif x < 0 and y < 0:
+    print ('3')
+elif x > 0 and y < 0:
+    print ('4')
+else:
+    print('dot is on an axis') 
