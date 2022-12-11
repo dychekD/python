ad5602f4fe404bfd547111f0447628d4322b4cb2
Daria Dychek <dychek.d@gmail.com>
2022-12-11 20:09:19 +0400

    modified task 11

---
+++ hwtask24.py
@@ -0,0 +1,15 @@
+# 1'. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# *Пример:*
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+
+list_for_odd = list(map(int, input('enter list of integers separated by space: ').split()))
+
+#v1
+summa = 0
+for i in range (1, len(list_for_odd)-1, 2):
+    summa += list_for_odd [i]
+print (f'sum of elements with odd indexes equals {summa}')
+
+#v2
+print (f'sum of elements with odd indexes equals {sum([list_for_odd [i] for i in range (1, len(list_for_odd)-1, 2)])}')
